192a48c515548bfd9d293b82c86950e57ec7efc4
deibertf <felix.deibert@brocodes.de>
2018-12-21 11:52:58 +0100

    added sample and images

---
+++ Sample/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ControlPermissions;
+
+namespace Sample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application, IPermissionControlApplication
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            var window = new MainWindow();
+            window.Title = $"MainWindow - [{GetUserType(Environment.UserName)}]";
+            MainWindow = window;
+            MainWindow.Show();
+        }
+
+        public UserType GetUserType(string username)
+        {
+            return UserType.Administrator;
+        }
+    }
+}
